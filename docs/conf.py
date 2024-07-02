c3d15cb92719d88ed629cbce799e60ae7b5d4897
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-07-02 19:30:55 +0200

    updated doc configuration for proper theme, inclusion of jupyter notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,59 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Electron Emission Fitter'
-copyright = '2024, Adrien Plaçais'
-author = 'Adrien Plaçais'
-release = '0.0.1'
+import os
+import re
+import sys
+
+sys.path.insert(
+    0,
+    os.path.abspath(
+        "/home/placais/Documents/Simulation/python/electron_emission_fitter/electron_emission_fitter/"
+    ),
+)
+project = "Electron Emission Fitter"
+copyright = "2024, Adrien Plaçais"
+author = "Adrien Plaçais"
+release = "0.0.1"
+
+# See https://protips.readthedocs.io/git-tag-version.html
+# The full version, including alpha/beta/rc tags.
+release = re.sub("^v", "", os.popen("git describe").read().strip())
+# The short X.Y version.
+version = release
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.napoleon",  # handle numpy style
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",  # ReadTheDocs theme
+    "myst_parser",  # still useful?
+    "sphinx.ext.todo",  # allow use of TODO
+    # "sphinx.ext.viewcode",
+    "nbsphinx",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": True,
+    "special-members": "__init__, __post_init__",
+}
 
+add_module_names = False
+default_role = "literal"
+todo_include_todos = True
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "display_version": True,
+}
+html_static_path = ["_static"]
