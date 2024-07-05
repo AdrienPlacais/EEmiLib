27da505411ec8023203c9b508bbc3eac2f7cd1f1
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-07-05 09:23:23 +0200

    first working basic plotter

---
+++ eemilib/plotter/pandas.py
@@ -0,0 +1,56 @@
+"""Define plotter relying on pandas."""
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+import numpy as np
+import pandas as pd
+from matplotlib.axes import Axes
+
+from eemilib.emission_data.emission_yield import EmissionYield
+from eemilib.loader.loader import EY_col1
+from eemilib.model.model import Model
+from eemilib.plotter.plotter import Plotter
+
+
+class PandasPlotter(Plotter):
+    """A :class:`.Plotter` using pandas lib."""
+
+    def plot_emission_yield_data(
+        self, emission_yield: EmissionYield, axes: Axes | None = None, **kwargs
+    ) -> Axes:
+        """Plot :class:`.EmissionYield` data."""
+        axes = emission_yield.data.plot(
+            x=EY_col1,
+            ax=axes,
+            grid=True,
+            ylabel=emission_yield.label,
+            **kwargs,
+        )
+        return axes
+
+    def plot_emission_yield_model(
+        self,
+        model: Model,
+        energies: np.ndarray,
+        angles: np.ndarray,
+        axes: Axes | None = None,
+        **kwargs,
+    ) -> Axes:
+        """Plot the given emission yield, return Axes object."""
+        if axes is not None:
+            axes.set_prop_cycle(None)
+        to_plot = model.teey(energies, angles)
+        to_plot.plot(x=EY_col1, ax=axes, ls="--", grid=True, **kwargs)
+
+    def plot_emission_energy_distribution(
+        self, emission_energies: Any, distributions: Any, **kwargs
+    ) -> object:
+        """Plot the given emission energy distribution, return Axes object."""
+        raise NotImplementedError
+
+    def plot_emission_angle_distribution(
+        self, emission_angles: Any, distributions: Any, **kwargs
+    ) -> object:
+        """Plot the given emission angles distribution, return Axes object."""
+        raise NotImplementedError
--- eemilib/plotter/plotter.py
+++ eemilib/plotter/plotter.py
@@ -3,14 +3,34 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+import numpy as np
+
+from eemilib.emission_data.emission_yield import EmissionYield
+from eemilib.model.model import Model
+
 
 class Plotter(ABC):
     """A generic object to plot distributions, emission yields, etc."""
 
     @abstractmethod
-    def plot_emission_yield(
-        self, energies: Any, yields: Any, **kwargs
-    ) -> object:
+    def plot_emission_yield_data[
+        T
+    ](
+        self, emission_yield: EmissionYield, axes: T | None = None, **kwargs
+    ) -> T:
+        """Plot :class:`.EmissionYield` data."""
+
+    @abstractmethod
+    def plot_emission_yield_model[
+        T
+    ](
+        self,
+        model: Model,
+        energies: np.ndarray,
+        angles: np.ndarray,
+        axes: T | None = None,
+        **kwargs,
+    ) -> T:
         """Plot the given emission yield, return Axes object."""
 
     @abstractmethod
