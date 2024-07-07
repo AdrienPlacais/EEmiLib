d8b1c76372e5565587db685425345725773edca6
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-07-07 19:46:42 +0200

    added to constants the list of file types for emission data

--- eemilib/util/constants.py
+++ eemilib/util/constants.py
@@ -2,6 +2,9 @@
 
 from typing import Literal
 
+ImplementedEmissionData = Literal[
+    "Emission Yield", "Emission Energy", "Emission Angle"
+]
 ImplementedPop = Literal["SE", "EBE", "IBE", "all"]
 EY_col_energy = "Energy [eV]"
 EY_col_normal = "0.0 [deg]"
