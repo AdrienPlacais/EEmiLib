a0e8b2809c187762f0a22b434901eff2d8e51407
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-07-10 17:00:42 +0200

    changed directory structure

---
+++ src/eemilib/__init__.py
@@ -0,0 +1,7 @@
+"""EEmiLib (Electron EMIssion Library) holds several electron emission models and offers a simple way to fit the on electron emission data."""
+
+# from .emission_data.data_matrix import DataMatrix
+# from .main import main
+# from .model.vaughan import Vaughan
+# from .util.constants import IMPLEMENTED_EMISSION_DATA, IMPLEMENTED_POP
+# from .util.helper import get_classes
