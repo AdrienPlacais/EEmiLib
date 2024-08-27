fad0bb57349ed3df8ce490c2919d4d90753df5f5
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-08-27 11:50:11 -0500

    with new handling of documentation and help button

--- html/_sources/eemilib/eemilib.ui.rst.txt
+++
@@ -1,7 +0,0 @@
-eemilib.ui package
-==================
-
-.. automodule:: eemilib.ui
-   :members:
-   :undoc-members:
-   :show-inheritance:
--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.2-3-ga95a023',
+    VERSION: '0.0.3-5-g96defe6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
