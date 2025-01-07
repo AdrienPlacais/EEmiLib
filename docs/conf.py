91dbd2dc13c61c40a090458ddf494bd0a557a706
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-01-07 14:40:45 +0100

    fixed missing types for doc

--- docs/conf.py
+++ docs/conf.py
@@ -7,38 +7,65 @@ version = eemilib.__version__
 release = version
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    "sphinxcontrib.bibtex",
-    "sphinx.ext.napoleon",  # handle numpy style
-    "sphinx.ext.autodoc",
-    "sphinx_rtd_theme",  # ReadTheDocs theme
-    "myst_parser",  # still useful?
-    "sphinx.ext.todo",  # allow use of TODO
-    # "sphinx.ext.viewcode",
+    "myst_parser",
     "nbsphinx",
+    "sphinx.ext.autodoc",  # include doc from docstrings
+    "sphinx.ext.intersphinx",  # interlink with other docs, such as numpy
+    "sphinx.ext.napoleon",  # handle numpy style
+    "sphinx.ext.todo",  # allow use of TODO
+    "sphinx_rtd_theme",  # ReadTheDocs theme
+    "sphinxcontrib.bibtex",
 ]
-bibtex_bibfiles = ["references.bib"]
 
 autodoc_default_options = {
+    "member-order": "bysource",
     "members": True,
-    "undoc-members": True,
     "private-members": True,
-    "special-members": "__init__, __post_init__",
+    "special-members": "__init__, __post_init__, __str__",
+    "undoc-members": True,
 }
 
 add_module_names = False
 default_role = "literal"
 todo_include_todos = True
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+bibtex_bibfiles = ["references.bib"]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "display_version": True,
 }
 html_static_path = ["_static"]
+
+# -- Check that there is no broken link --------------------------------------
+nitpicky = True
+nitpick_ignore = [
+    ("py:class", "PyQt5.QtWidgets.QCheckBox"),
+    ("py:class", "PyQt5.QtWidgets.QGroupBox"),
+    ("py:class", "PyQt5.QtWidgets.QHBoxLayout"),
+    ("py:class", "PyQt5.QtWidgets.QLineEdit"),
+    ("py:class", "PyQt5.QtWidgets.QListWidget"),
+    ("py:class", "PyQt5.QtWidgets.QMainWindow"),
+    ("py:class", "PyQt5.QtWidgets.QPushButton"),
+    ("py:class", "PyQt5.QtWidgets.QRadioButton"),
+    ("py:class", "PyQt5.QtWidgets.QTableWidget"),
+    ("py:class", "PyQt5.QtWidgets.QTableWidgetItem"),
+    ("py:class", "PyQt5.QtWidgets.QWidget"),
+    ("py:class", "T"),
+    ("py:class", "optional"),
+]
+intersphinx_mapping = {
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs", None),
+    "python": ("https://docs.python.org/3", None),
+    "PyQt5": (
+        "https://www.riverbankcomputing.com/static/Docs/PyQt5/",
+        None,
+    ),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+}
