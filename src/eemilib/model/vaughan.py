AdrienPlacais/EEmiLib

commit b9901f8b0b789e7e7515d3a4ef7347f9cb56f888

better error handling. now we can fit E_0 even without file

src/eemilib/model/vaughan.py
@@ -196,7 +196,13 @@ class Vaughan(Model):
         self, data_matrix: DataMatrix, **kwargs
     ) -> None:
         """Match with position of first crossover and maximum."""
-        data_matrix.assert_has_all_mandatory_files(self.model_config)
+        if not data_matrix.has_all_mandatory_files(self.model_config):
+            logging.info(
+                "Files are not all provided. If Ec1 was given, I will try to "
+                "find the corresponding E_0."
+            )
+            self.find_e_0()
+            return
 
         emission_yield = data_matrix.data_matrix[3][0]
         assert isinstance(
@@ -215,6 +221,19 @@ class Vaughan(Model):
         if not self.parameters["E_0"].is_locked:
             E_0 = self._retrieve_E_0(self.parameters["E_c1"].value)
             self.set_parameter_value("E_0", E_0)
+
+    def find_e_0(self) -> None:
+        """Find E_0 with error handling."""
+        e_0 = self.parameters["E_0"]
+        assert not e_0.is_locked, "Unlock E_0 to allow for a fit."
+
+        for key in ("E_c1", "E_max", "teey_max"):
+            value = self.parameters[key].value
+            assert value is not None, f"You must provide a value for {key}"
+
+        E_0 = self._retrieve_E_0(self.parameters["E_c1"].value)
+        self.set_parameter_value("E_0", E_0)
+        return
 
     def _retrieve_E_0(self, E_c1: float) -> float:
         """Fit E_0 to retrieve E_c1 (SPARK3D)"""

src/eemilib/emission_data/data_matrix.py
@@ -5,6 +5,7 @@
 
 """
 
+import logging
 from collections.abc import Collection
 from pathlib import Path
 from typing import Literal, overload
@@ -340,9 +341,7 @@ class DataMatrix:
                         emission_data_type=data_type,
                     )  # type: ignore
 
-    def assert_has_all_mandatory_files(
-        self, model_config: ModelConfig
-    ) -> None:
+    def has_all_mandatory_files(self, model_config: ModelConfig) -> bool:
         """Tell if files defined by :attr:`.Model.model_config` are set."""
         for emission_data_type, corresponding_attribute in zip(
             IMPLEMENTED_EMISSION_DATA,
@@ -357,23 +356,35 @@ class DataMatrix:
             )
 
             for mandatory_population in mandatory_populations:
-                assert mandatory_population in IMPLEMENTED_POP
+                if mandatory_population not in IMPLEMENTED_POP:
+                    logging.error(
+                        f"{mandatory_population = } not in "
+                        f"{IMPLEMENTED_POP = }"
+                    )
+                    return False
+
                 filepath = self.get_files(
                     population=mandatory_population,
                     emission_data_type=emission_data_type,
                 )  # type: ignore
-                assert filepath is not None, (
-                    f"You must define a {emission_data_type} filepath for"
-                    + f" population {mandatory_population}"
-                )
+                if filepath is None:
+                    logging.error(
+                        f"You must define a {emission_data_type} filepath for"
+                        + f" population {mandatory_population}"
+                    )
+                    return False
+
                 data_object = self.get_data(
                     population=mandatory_population,
                     emission_data_type=emission_data_type,
                 )  # type: ignore
-                assert data_object is not None, (
-                    f"You must load {emission_data_type} filepath for "
-                    + f"population {mandatory_population}"
-                )
+                if data_object is None:
+                    logging.error(
+                        f"You must load {emission_data_type} filepath for "
+                        + f"population {mandatory_population}"
+                    )
+                    return False
+        return True
 
     def plot[
         T

commit ae4c7a542b169a5bdae2403c1cbc83f59df6defc

fixed bug introduced in b9901f8

src/eemilib/model/vaughan.py
@@ -168,7 +168,7 @@ class Vaughan(Model):
             )
             self.parameters["E_0"].is_locked = False
 
-            E_0 = self._retrieve_E_0(self.parameters["E_c1"].value)
+            E_0 = self._E_0_matching(E_c1=self.parameters["E_c1"].value)
             if np.isnan(E_0):
                 return
             self.set_parameter_value("E_0", E_0)
@@ -219,7 +219,7 @@ class Vaughan(Model):
         if not self.parameters["E_c1"].is_locked:
             self.set_parameter_value("E_c1", emission_yield.e_c1)
         if not self.parameters["E_0"].is_locked:
-            E_0 = self._retrieve_E_0(self.parameters["E_c1"].value)
+            E_0 = self._E_0_matching(E_c1=self.parameters["E_c1"].value)
             self.set_parameter_value("E_0", E_0)
 
     def find_e_0(self) -> None:
@@ -231,11 +231,11 @@ class Vaughan(Model):
             value = self.parameters[key].value
             assert value is not None, f"You must provide a value for {key}"
 
-        E_0 = self._retrieve_E_0(self.parameters["E_c1"].value)
+        E_0 = self._E_0_matching(E_c1=self.parameters["E_c1"].value)
         self.set_parameter_value("E_0", E_0)
         return
 
-    def _retrieve_E_0(self, E_c1: float) -> float:
+    def _E_0_matching(self, *, E_c1: float) -> float:
         """Fit E_0 to retrieve E_c1 (SPARK3D)"""
         parameters = self.parameters.copy()
 

tests/model/test_vaughan.py
@@ -25,9 +25,9 @@ class MockDataMatrix(DataMatrix):
             [emission_data, None, None],
         ]
 
-    def assert_has_all_mandatory_files(self, *args, **kwargs) -> None:
+    def has_all_mandatory_files(self, *args, **kwargs) -> bool:
         """Skip this check."""
-        pass
+        return True
 
 
 def test_initial_parameters(vaughan_model: Vaughan) -> None: