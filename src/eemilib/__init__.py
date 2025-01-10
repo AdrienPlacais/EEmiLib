f603848c8476422808b1f8f6e12c79b1fb405292
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-01-10 10:57:56 +0100

    set up basic logging

--- src/eemilib/__init__.py
+++ src/eemilib/__init__.py
@@ -6,5 +6,9 @@ offers a simple way to fit the on electron emission data.
 
 import importlib.metadata
 
+from eemilib.util.log_manager import set_up_logging
+
 DOC_URL = "https://adrienplacais.github.io/EEmiLib/"
 __version__ = importlib.metadata.version("eemilib")
+
+set_up_logging("EEmiLib")
