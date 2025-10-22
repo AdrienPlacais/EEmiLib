a1efe326b4e59acb68b27559635217d07d111bf4
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-10-22 16:12:12 +0200

    fixed some bad re-import interactions with sphinx

--- src/eemilib/loader/__init__.py
+++ src/eemilib/loader/__init__.py
@@ -1,12 +1,6 @@
 """Define objects to load the different formats of electron emission files."""
 
-from .deesse_loader import DeesseLoader as _DeesseLoader
-from .pandas_loader import PandasLoader as _PandasLoader
-
-# Trick to avoid Sphinx duplicate reference warning
-_DeesseLoader.__module__ = "eemilib.loader"
-DeesseLoader = _DeesseLoader
-_PandasLoader.__module__ = "eemilib.loader"
-PandasLoader = _PandasLoader
+from .deesse_loader import DeesseLoader
+from .pandas_loader import PandasLoader
 
 __all__ = ["DeesseLoader", "PandasLoader"]
--- src/eemilib/plotter/__init__.py
+++ src/eemilib/plotter/__init__.py
@@ -1,9 +1,5 @@
 """Define the object that will take care of plotting the data."""
 
-from .pandas import PandasPlotter as _PandasPlotter
-
-# Trick to avoid Sphinx duplicate reference warning
-_PandasPlotter.__module__ = "eemilib.plotter"
-PandasPlotter = _PandasPlotter
+from .pandas import PandasPlotter
 
 __all__ = ["PandasPlotter"]
