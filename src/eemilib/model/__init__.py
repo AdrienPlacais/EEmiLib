f13f22fb6d6649ee7f934d71b179adf42ae1a85d
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-08 13:26:59 +0200

    made `FurmanPivi` available from `eemilib.model`

--- src/eemilib/model/__init__.py
+++ src/eemilib/model/__init__.py
@@ -2,8 +2,16 @@
 
 from .chung_and_everhart import ChungEverhart
 from .dionne import Dionne
+from .furman_pivi import FurmanPivi
 from .maxwellian import Maxwellian
 from .sombrin import Sombrin
 from .vaughan import Vaughan
 
-__all__ = ["ChungEverhart", "Dionne", "Maxwellian", "Sombrin", "Vaughan"]
+__all__ = [
+    "ChungEverhart",
+    "Dionne",
+    "FurmanPivi",
+    "Maxwellian",
+    "Sombrin",
+    "Vaughan",
+]
