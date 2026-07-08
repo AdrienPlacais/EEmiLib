763d87290d1529a9a6bfb38cbe6ded3a891e7ab9
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-08 13:04:09 +0200

    `Plotter.population_styles` and `Plotter.is_model_styles` for consistent plot kwargs

--- src/eemilib/plotter/plotter.py
+++ src/eemilib/plotter/plotter.py
@@ -6,9 +6,29 @@ import pandas as pd
 from eemilib.util.constants import ImplementedPop
 from eemilib.util.helper import documentation_url
 
+DEFAULT_POPULATIONS_STYLES: dict[ImplementedPop, dict[str, str | float]] = {
+    "all": {"ls": "-"},
+    "SE": {"ls": "--"},
+    "EBE": {"ls": "-."},
+    "IBE": {"ls": ":"},
+}
+DEFAULT_IS_MODEL_STYLES: dict[bool, dict[str, str | float]] = {
+    False: {"marker": "+", "lw": 0.5},
+    True: {"marker": ""},
+}
+
 
 class Plotter(ABC):
     """A generic object to plot distributions, emission yields, etc."""
+
+    #: Determine plot styles according to population nature.
+    population_styles: dict[ImplementedPop, dict[str, str | float]] = (
+        DEFAULT_POPULATIONS_STYLES
+    )
+    #: Determine plot styles according to whether data is modelled or measured.
+    is_model_styles: dict[bool, dict[str, str | float]] = (
+        DEFAULT_IS_MODEL_STYLES
+    )
 
     def __init__(self, *args, gui: bool = False, **kwargs) -> None:
         """Instantiate the object.
@@ -27,6 +47,7 @@ class Plotter(ABC):
         df: pd.DataFrame,
         axes: T | None = None,
         population: ImplementedPop | None = None,
+        is_model: bool = True,
         **kwargs,
     ) -> T:
         """Plot emission yield data."""
@@ -37,6 +58,7 @@ class Plotter(ABC):
         df: pd.DataFrame,
         axes: T | None = None,
         population: ImplementedPop | None = None,
+        is_model: bool = True,
         **kwargs,
     ) -> T:
         """Plot the given emission energy distribution, return Axes object."""
@@ -47,6 +69,7 @@ class Plotter(ABC):
         df: pd.DataFrame,
         axes: T | None = None,
         population: ImplementedPop | None = None,
+        is_model: bool = True,
         **kwargs,
     ) -> T:
         """Plot the given emission angles distribution, return Axes object."""
