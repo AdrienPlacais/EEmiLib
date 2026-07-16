b81ec01eb1778ffe1020e502c2dbf5f297e094f4
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-16 12:40:12 +0200

    made `CSTLoader` available from `loader`

--- src/eemilib/loader/__init__.py
+++ src/eemilib/loader/__init__.py
@@ -1,6 +1,7 @@
 """Define objects to load the different formats of electron emission files."""
 
+from .cst_loader import CSTLoader
 from .deesse_loader import DeesseLoader
 from .pandas_loader import PandasLoader
 
-__all__ = ["DeesseLoader", "PandasLoader"]
+__all__ = ["CSTLoader", "DeesseLoader", "PandasLoader"]
