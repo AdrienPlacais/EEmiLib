ec0daa4a3f1cc5548f076e4a6ed3198e656cdfb1
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-16 14:11:02 +0200

    properly packaged data

---
+++ src/eemilib/data/ag/emission_energy/__init__.py
@@ -0,0 +1,34 @@
+"""
+Emission energy measurements realized at ONERA, Toulouse, France using the
+DEESSE test facility on 2018-06-04.
+
+The Ag sample was heated at 200 celsius during 2 hours.
+
+It is called "heated Ag#1" in my PhD :cite:`Placais2021`. Emission energy data
+was corrected using M. Villemant procedure :cite:`Villemant2018`.
+
+This data can be loaded with :class:`.pandas_loader.PandasLoader`.
+
+"""
+
+from importlib import resources
+
+files = resources.files(__name__)
+
+distrib_10eV = files / "corrected_cleanAg0_10eV_2018.05.30.csv"
+distrib_20eV = files / "corrected_cleanAg0_20eV_2018.05.30.csv"
+distrib_30eV = files / "corrected_cleanAg0_30eV_2018.05.30.csv"
+distrib_50eV = files / "corrected_cleanAg0_50eV_2018.05.30.csv"
+distrib_70eV = files / "corrected_cleanAg0_70eV_2018.05.30.csv"
+distrib_100eV = files / "corrected_cleanAg0_100eV_2018.05.30.csv"
+distrib_150eV = files / "corrected_cleanAg0_150eV_2018.05.30.csv"
+
+distribs = (
+    distrib_10eV,
+    distrib_20eV,
+    distrib_30eV,
+    distrib_50eV,
+    distrib_70eV,
+    distrib_100eV,
+    distrib_150eV,
+)
--- src/eemilib/data/reference_ag/__init__.py
+++ src/eemilib/data/reference_ag/__init__.py
@@ -7,3 +7,8 @@ this reference if you use it in your work.
 This data can be loaded with :class:`.pandas_loader.PandasLoader`.
 
 """
+
+from importlib import resources
+
+files = resources.files(__name__)
+emission_yield = files / "K-S8_AG_TECHNICAL_TEEY_REF.csv"
