51de44cac594610dc7690eba65596ac287a14d68
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-19 16:26:11 +0200

    new `Plotter.plot` method that calls the appropriate plotting method

--- src/eemilib/plotter/plotter.py
+++ src/eemilib/plotter/plotter.py
@@ -3,7 +3,7 @@
 from abc import ABC, abstractmethod
 
 import pandas as pd
-from eemilib.util.constants import ImplementedPop
+from eemilib.util.constants import ImplementedEmissionData, ImplementedPop
 from eemilib.util.helper import documentation_url
 
 DEFAULT_POPULATIONS_STYLES: dict[ImplementedPop, dict[str, str | float]] = {
@@ -40,6 +40,42 @@ class Plotter(ABC):
 
         """
         self.doc_url = documentation_url(self)
+
+    def plot[T](
+        self,
+        emission_data_type: ImplementedEmissionData,
+        df: pd.DataFrame,
+        axes: T | None = None,
+        population: ImplementedPop | None = None,
+        is_model: bool = True,
+        **kwargs,
+    ) -> T:
+        """Call the appropriate plot method.
+
+        See Also
+        --------
+        :meth:`.plot_emission_yield`
+        :meth:`.plot_emission_energy_distribution`
+        :meth:`.plot_emission_angle_distribution`
+
+        """
+        if emission_data_type == "Emission Yield":
+            meth = self.plot_emission_yield
+        elif emission_data_type == "Emission Energy":
+            meth = self.plot_emission_energy_distribution
+        elif emission_data_type == "Emission Angle":
+            meth = self.plot_emission_angle_distribution
+        else:
+            raise ValueError(
+                f"{emission_data_type = } should be in {ImplementedEmissionData}."
+            )
+        return meth(
+            df=df,
+            axes=axes,
+            population=population,
+            is_model=is_model,
+            **kwargs,
+        )
 
     @abstractmethod
     def plot_emission_yield[T](
