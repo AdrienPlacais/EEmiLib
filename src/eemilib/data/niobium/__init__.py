2e362a4e1afef15f10b01654b998001d895fc701
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-20 09:38:09 +0200

    packaged Nb data

---
+++ src/eemilib/data/niobium/__init__.py
@@ -0,0 +1,18 @@
+r"""This is data for niobium.
+
+It was manually extracted from following papers:
+- :math:`300\,\mathrm{\degrees}` baked out niobium: :cite:`Piel1988`.
+- Degreased niobium: :cite:`Aull2015`.
+- Sputtered niobium: :cite:`Aull2015`.
+
+This data can be loaded using :class:`.PandasLoader`.
+
+"""
+
+from importlib import resources
+
+files = resources.files(__name__)
+
+nb_baked_out = files / "Nb_baked-out-at-300_CERN_WPD.csv"
+nb_degreased = files / "Nb_degreased_WPD.csv"
+nb_sputtered = files / "Nb_sputtered_WPD.csv"
