6927d43e7c2ad63a3f1de8cc0e2b0266a11767d6
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2026-07-24 16:42:14 +0200

    basic example for fitting FP

---
+++ examples/test_furman_pivi.py
@@ -0,0 +1,69 @@
+"""
+
+
+==========
+
+This script showcases the basics of loading data and fitting a model.
+
+"""
+
+import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+from eemilib.data.ag.emission_energy import distribs
+from eemilib.data.ag.emission_yield import teey_ag_1_heated_pd
+from eemilib.emission_data import DataMatrix
+from eemilib.loader import PandasLoader
+from eemilib.model import FurmanPivi
+from eemilib.plotter import PandasPlotter
+
+# mylog = logging.getLogger()
+# myconsolehandler = mylog.handlers[0]
+# myconsolehandler.setLevel("DEBUG")
+
+data_matrix = DataMatrix()
+loader = PandasLoader()
+plotter = PandasPlotter()
+model = FurmanPivi()
+
+data_matrix.set_files(
+    teey_ag_1_heated_pd, population="all", emission_data_type="Emission Yield"
+)
+data_matrix.set_files(
+    distribs, population="all", emission_data_type="Emission Energy"
+)
+data_matrix.load_data(loader)
+
+
+axes_distrib = data_matrix.plot(
+    plotter,
+    population="all",
+    emission_data_type="Emission Energy",
+    group_by_pe=True,
+)
+axes_yield = data_matrix.plot(
+    plotter, population="all", emission_data_type="Emission Yield"
+)
+
+model.find_optimal_parameters(data_matrix)
+
+axes_distrib = model.plot(
+    plotter,
+    population=["SE", "EBE", "IBE", "all"],
+    emission_data_type="Emission Energy",
+    energies=np.linspace(0, 500, 501),
+    angles=np.linspace(0, 0, 1),
+    axes=axes_distrib,
+    group_by_pe=True,
+)
+
+axes_yield = model.plot(
+    plotter,
+    population=["SE", "EBE", "IBE", "all"],
+    emission_data_type="Emission Yield",
+    energies=np.linspace(0, 1000, 501),
+    angles=np.linspace(0, 60, 4),
+    axes=axes_yield,
+)
+plt.show()
